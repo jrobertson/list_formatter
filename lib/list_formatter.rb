1f2df2867938fea29864d072e8b6f8a73c6a77bd
Your Name <you@example.com>
2015-11-11 00:57:30 +0000

    feature: implemented word wrap

--- lib/list_formatter.rb
+++ lib/list_formatter.rb
@@ -10,10 +10,10 @@ class ListFormatter
     
     top = "== #{title} ".ljust(width,'=')
     bottom = '=' * width
-     
-    @to_s = top + "\n\n" \
+    s = top + "\n\n" \
        + format_rows(a, layout, field_marker).join("\n" + '-' * width \
        + "\n\n") + "\n" + bottom
+    @to_s = printout(wordwrap(s, width),width)
 
   end
 
@@ -30,4 +30,38 @@ class ListFormatter
     end
 
   end
+
+
+  def printout(s, width=32)
+
+    a2 = (" \n" + s + "\n ").lines.inject([]) do |r,x| 
+      r + x.chomp.each_char.each_slice(width).map do |row|
+        "%s" % (row.join + ' ' * width).slice(0,width)
+      end
+    end
+    "%s" % [a2.join("\n")]
+  end
+    
+  def wordwrap(s, cols=32)
+
+    a = s.split(/ /).inject(['']) do |r,word|
+
+      word.lines.each do |x|
+
+        if (r[-1] + x).length <= cols then
+          r[-1] << (r[-1].empty? ? x : ' ' + x)
+        else
+          r +=  [x]
+        end
+        r[-1] = r[-1].chomp ; r += [''] if x[-1] == "\n"
+
+      end
+      r
+    end
+
+    a.join("\n")
+
+  end
+
+  
 end
